4118c75e3077d14eddc8e0c5d2b049c824bda9d3
sharim shaikh <109298437+Sharim0684@users.noreply.github.com>
2024-08-22 05:53:08 +0000

    basic

---
+++ 01_Introduction/1.py
@@ -0,0 +1,19 @@
+print(2+3)  # 5
+print(3-1)  # 2
+print(2*3)  # 6
+print(3/2)  # 1.5
+print(3 ** 2) # 9
+print(3%2)    # 1
+print( 3 // 2) # 1
+
+
+# checking data types
+
+print(type(10))   #int
+print(type(3.14)) # float
+print(type(1+3j)) #complex
+print(type('hello')) #str
+print(type([1,2,3])) #list
+print(type({'name':'Sharim'}))  #dictionary
+print(type({9.8,3.14,3}))  # set
+print(type((9.8,3.14,2.7))) # tuple
